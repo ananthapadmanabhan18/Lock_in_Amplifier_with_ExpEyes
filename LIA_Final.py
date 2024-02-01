65f02e5c8090293e28ae441d6a0cee69ae5747a6
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2024-02-01 21:58:08 +0530

    Added loop to avg 10 samples

--- LIA_Final.py
+++ LIA_Final.py
@@ -3,6 +3,7 @@ import math as m
 from scipy.fft import fft, ifft,fftfreq
 import eyes17.eyes
 p=eyes17.eyes.open()
+import time as t
 
 ################################ Lock in Amplifier ########################################
 def LIA(V_in,t_in,f):   #V_in is the input voltage, t_in is the time list (in S),
@@ -25,39 +26,42 @@ def collect_signal(stri,N_sample,N_div,f):
 ############################################################################################
 
 
-f=2000
+f=200
 N_sample=8192
 N_div=128
 
-p.set_sine_amp(2)
-p.set_sine(f)
-
-t_in,V_in=collect_signal('A1',N_sample,N_div,f)
-t_out_opamp,V_out_opamp=collect_signal('A2',N_sample,N_div,f)
+# p.set_sine_amp(2)
+# p.set_sine(f)
 
 
+V1=[]
+V2=[]
 
 
-#RMS of V_in and t_in list
-V_in_rms = np.sqrt(np.mean(np.square(V_in)))
-print("The RMS of the input voltage is",V_in_rms,"V")
+for i in range(5):
+    t_in,V_in=collect_signal('A1',N_sample,N_div,f)
+    t_out_opamp,V_out_opamp=collect_signal('A2',N_sample,N_div,f)
+
+    V_in_rms = np.sqrt(np.mean(np.square(V_in))) 
+    V1.append(V_in_rms)
+    # print("The RMS of the input voltage is",V_in_rms,"V")
+    V_DC = LIA(V_out_opamp,t_out_opamp,f)
+    # print("The output voltage is",V_DC,"V")
+    V_DC=V_DC/np.sqrt(2)
+    V2.append(V_DC)
+    print(i+1)
+    t.sleep(1)
+
+
+V_out_avg=np.mean(V2)
+V_in_rms_avg=np.mean(V1)
+print("The RMS of the input voltage is",V_in_rms_avg,"V")
+print("The output voltage is",V_out_avg,"V")
+
+
+with open(f'D:\\Google_Drive\\Files\\My Drive\\Study_Stuff\\Semester-8\\Integrated_Laborotary_II\\Experiment_1_LockInAmplifier_using_Expeyes\\Codes_for_Expeyes\\Data\\MI_avg_dat_{f}Hz_fn.txt','a') as file:
+    file.write(str(V_in_rms_avg)+"\t"+str(V_out_avg)+"\n")
 
 
 
 
-V_DC = LIA(V_out_opamp,t_out_opamp,f)
-print("The output voltage is",V_DC,"V")
-V_DC=V_DC/np.sqrt(2)
-
-#open LIA.txt
-
-with open(f'D:\\Google_Drive\\Files\\My Drive\\Study_Stuff\\Semester-8\\Integrated_Laborotary_II\\Experiment_1_LockInAmplifier_using_Expeyes\\Codes_for_Expeyes\\Data\\MI_dat_{f}Hz_fn.txt','a') as file:
-    file.write(str(V_in_rms)+"\t"+str(V_DC)+"\n")
-
-
-
-# import matplotlib.pyplot as plt
-# plt.plot(t_in,V_in)
-# plt.plot(t_out_opamp,V_out_opamp)
-# plt.xlim(0,0.005)
-# plt.show()
