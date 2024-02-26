240dc0cb2d97ac04916d33b2f45b04bb36b0aaf3
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2024-02-26 19:43:30 +0530

    Doing CV

--- LIA_CV.py
+++ LIA_CV.py
@@ -23,58 +23,50 @@ def LIA(V_in,t_in,f):   #V_in is the input voltage, t_in is the time list (in S)
 #################### function to collect the signal from Expeyes ###########################
 def collect_signal(stri,N_sample,N_div,f):
         t_gap = (1/(f*N_div))*10**6 #us
-        t,v = p.capture1(stri,10000,2)
-        print('Time gap:',t_gap,'us')
+        t,v = p.capture1(stri,8192,2)
+        # print('Time gap:',t_gap,'us')
         return t/1000,v
 ############################################################################################
 
 ############################# Defining input Parameters ####################################
-f=600
+f=3000
 N_sample=8192
 N_div=128
 p.set_sine_amp(2)
 p.set_sine(f)
 p.set_pv1(1)
 ############################################################################################
-vset=[0.25,0.5,0.75,1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,3.25,3.5]
-vxlist_sd=[]
-vylist_sd=[]
+# vset=[0.25,0.75,1.25,1.75,2.25,2.75,3.25,3.5]
 
-vxlist_cc=[]
-vylist_cc=[]
+# vset =[0.2,0.3,0.4,0.5,0.6,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,2.1]
+vset =[]
+for i in range(2,20):
+    vset.append(i/10)
+# vset =[0.75]
+# vset =[1.25]
+# vset =[1.75]
+# vset =[2.25]
+# vset =[2.75]
+# vset =[3.25]
+# vset =[3.5]
 
 
-
-
-
-
-
-
+j=0
 for i in vset:
     p.set_pv1(i)
-    tim.sleep(1)
-    t,v=collect_signal('A2',N_sample,N_div,f)
+    tim.sleep(3)
+    t,v=collect_signal('A1',N_sample,N_div,f)
+    tim.sleep(3)
     Vx,Vy=LIA(v,t,f)
-    vxlist_sd.append(Vx)   
-    vylist_sd.append(Vy)
-  
+    print(str(vset[j])+'\t'+str(Vx)+'\t'+str(Vy))
+    with open('Data\\CC.txt','a') as file1:
+        file1.write(str(vset[j])+'\t'+str(Vx)+'\t'+str(Vy)+'\n')
+    j=j+1
 
 
-with open('Data\\solar_cc.txt','w') as f:
-    for i in range(len(vset)):
-        f.write(str(vset[i])+'\t'+str(vxlist_sd[i])+'\t'+str(vylist_sd[i])+'\n')
-    f.close()
 
+# with open('Data\\CV_CC.txt','w') as file1:
+#     for i in range(len(vset)):
+#         file1.write(str(vset[i])+'\t'+str(vxlist_sd[i])+'\t'+str(vylist_sd[i])+'\n')
+#     file1.close()
 
-# t1,v1=collect_signal('A2',N_sample,N_div,f)
-# t,v=collect_signal('A1',N_sample,N_div,f)
-# vout=LIA(v,t,f)
-# print(vout)
-
-# import matplotlib.pyplot as plt
-# plt.plot(t1,v1,label='SD')
-# plt.plot(t,v,label='CC')
-# plt.legend()
-# plt.grid()
-# plt.xlim(0,0.01)
-# plt.show()
